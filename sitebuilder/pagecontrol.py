a4b7422f3477cf4c6f028c006c1926705ccd119f
Ccamm <alex.brown.r@gmail.com>
2022-10-22 16:56:02 +0800

    Fix rendering names and dates

--- sitebuilder/pagecontrol.py
+++ sitebuilder/pagecontrol.py
@@ -25,14 +25,15 @@ class PageControl:
     def render_pages(self, jinja_env: Environment, html_folder):
         for path in self.published_pages:
             page_config = self.page_configs[path]
+            release_date = page_config.get("release_date").strftime("%Y-%m-%d") if page_config.get("release_date", False) else ""
             html_page_folder = os.path.join(html_folder, path)
             os.makedirs(html_page_folder, exist_ok=True)
             html_path = os.path.join(html_page_folder, "index.html")
             template = jinja_env.get_template("article.html")
             template_str = template.render(
-                name = page_config["name"],
+                name = page_config["page_name"],
                 description = page_config["desc"],
-                date = page_config["release_date"],
+                date = release_date,
                 body = page_config["html"]
             )
             with open(html_path, "w") as f:
